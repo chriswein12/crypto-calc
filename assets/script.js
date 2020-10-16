439db9606aa8e0ac169ac4e902bf8e6ad3e0d0a4
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-10-16 00:36:51 -0500

    reduced code to barebones for easier merging. completed selector for api. Specifed assests to selected from.

--- assets/script.js
+++ assets/script.js
@@ -1,96 +1,56 @@
-// var elements to id html containers
-var cryptoFormEl = document.querySelector("#crypto-form");
-var nameInputEl = document.querySelector("#crypto")
- 
-// var containers to hold imput html data
+// var container to hold crypto data return
 var cryptoContainerEl = document.querySelector("#crypto-container")
-var cryptoSearchTerm = document.querySelector("#crypto-search-term")
-
-//Crypto coin button selector. 
-var coinButtonEl = document.querySelector("#coin-buttons");
-
 
 // var container to populate with crypto search data and fetch api
-// api key = P4A34BK1ZIK2J6PV
 var getCryptoData = function(crypto){
 
-    var apiUrl = `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=${crypto}&market=USD&apikey=P4A34BK1ZIK2J6PV`
+// creating url to call
+var apiUrl = `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=${crypto}&market=USD&apikey=P4A34BK1ZIK2J6PV`
     
-    fetch(apiUrl)
-    .then(function(response) {
-         // request was successful
-         if (response.ok){       
+// fetching created url
+fetch(apiUrl)
+.then(function(response) {
+    // request was successful
+    if (response.ok){       
             response.json().then(function(data){ 
-                displayStats(data, crypto);           
-                console.log(data);
+                displayStats(data);
+                // verifing dat is correct in console           
+                console.log(data);                
             });
             //error handler
         } else {
             alert("Error: " + response.statusText);
-        }
-        
+        }        
     }); 
-
 }
 
 // passing response data to html
-var displayStats = function(data, crypto) {
-   // displaying in html crypto name that was search 
-    var cryptoName = crypto;
-cryptoSearchTerm.textContent = cryptoName + " = ";
-
-// using moment to add current date. 
-var currentDate = moment().format("YYYY-MM-DD");
-console.log(currentDate);
-
- var cryptoClose = document.createElement("h2")
- cryptoClose.id = "coinPrice"
- cryptoClose.textContent = "$ " + data["Time Series (Digital Currency Daily)"][currentDate]["4a. close (USD)"];
-
-// dispalying crypto close on html
-cryptoContainerEl.appendChild(cryptoClose);
-
-console.log(cryptoClose);
-}
-
-//prevented refresh of browser
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    console.log(event);
-    
-   // removing searched crypto token from button element.  
-    var formSearch = document.getElementById("coinPrice")
+var displayStats = function(data) {
+// clearing previous search
+   var formSearch = document.getElementById("coinPrice")
     if (formSearch)
     formSearch.parentNode.removeChild(formSearch);
 
-// Var holding crypto name that was given
-var cryptoName = nameInputEl.value.trim();
-console.log(cryptoName);
+// using moment to add current date. 
+var currentDate = moment().format("YYYY-MM-DD");
+// verifing currentDate in console
+console.log(currentDate);
 
-if(cryptoName) {
-    getCryptoData(cryptoName);
-    nameInputEl.value = "";
-// handling invalid crypto names
-}else {
-    alert("Please enter Crypto Name")
-}
-}
-//var for coin button selector
-var buttonClickHandler = function(event) {
-    var coin = event.target.getAttribute("data-coin");
-    if (coin) {
-        getCryptoData(coin);
+// creating dom to display on index html
+var cryptoClose = document.createElement("h2")
+cryptoClose.id = "coinPrice"
+cryptoClose.textContent = "$ " + data["Time Series (Digital Currency Daily)"][currentDate]["4a. close (USD)"];
+ 
+// attaching crypto close on html container
+cryptoContainerEl.appendChild(cryptoClose);
 
-        //clear old content
-        cryptoContainerEl.textContent = "";
-    }
+// verifing closing price recieved in console 
+console.log(cryptoClose);
 }
 
-
-// event listener for crypto coin button
-coinButtonEl.addEventListener("click", buttonClickHandler);
-
-// event listener to call formSubmitHandler to start the process. 
-
-cryptoFormEl.addEventListener("submit", formSubmitHandler);
-
+// function called from html when selector is released. Indicates an "onchange."
+function copy() {
+    var cryptoSelected = document.getElementById("cryptoSelect").value
+    console.log(cryptoSelected);   
+    getCryptoData(cryptoSelected);
+}
