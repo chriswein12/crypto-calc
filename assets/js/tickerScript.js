4113302cf20ba7850abb8abd6bf5c8248cbf0858
Johnny-smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-10-19 22:58:32 -0500

    added two more cryptos to ticker and modified html styling

--- assets/js/tickerScript.js
+++ assets/js/tickerScript.js
@@ -4,6 +4,9 @@
 var cryptoExchangeElBtc = document.querySelector("#btc");
 var cryptoExchangeElEth = document.querySelector("#eth");
 var cryptoExchangeElXrp = document.querySelector("#xrp");
+var cryptoExchangeElUsdt = document.querySelector("#usdt");
+var cryptoExchangeElLtc = document.querySelector("#ltc");
+
 
 var tickerBtc = function(){
 var array = "btc";
@@ -23,6 +26,7 @@ fetch(realTimeApiUrl)
 })
 }
 
+// Creating html object to display
 var displayExchangeBtc = function(exchange) {
     // clearing previous search
     var formSearch = document.getElementById("coinPriceBtc")
@@ -30,9 +34,9 @@ var displayExchangeBtc = function(exchange) {
         formSearch.parentNode.removeChild(formSearch);
 
     // creating dom to display on index html
-    var cryptoExchangeBtc = document.createElement("span")
+    var cryptoExchangeBtc = document.createElement("h5")
     cryptoExchangeBtc.id = "coinPriceBtc"
-    cryptoExchangeBtc.textContent = "$ " + exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+    cryptoExchangeBtc.textContent = "$ " + Math.round( (exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"])*10000)/10000;
     
     // attaching crypto close on html container
     cryptoExchangeElBtc.appendChild(cryptoExchangeBtc);
@@ -58,6 +62,7 @@ var tickerEth = function(){
     })
     }
     
+    // Creating html object to display
     var displayExchangeEth = function(exchange) {
         // clearing previous search
         var formSearch = document.getElementById("coinPriceEth")
@@ -65,9 +70,9 @@ var tickerEth = function(){
             formSearch.parentNode.removeChild(formSearch);
     
         // creating dom to display on index html
-        var cryptoExchangeEth = document.createElement("span")
+        var cryptoExchangeEth = document.createElement("h5")
         cryptoExchangeEth.id = "coinPriceEth"
-        cryptoExchangeEth.textContent = "$ " + Math.round( (exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"])*100)/100;
+        cryptoExchangeEth.textContent = "$ " + Math.round( (exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"])*10000)/10000;
         
         // attaching crypto close on html container
         cryptoExchangeElEth.appendChild(cryptoExchangeEth);
@@ -94,6 +99,7 @@ var tickerXrp = function(){
    })
    }
    
+      // Creating html object to display
    var displayExchangeXrp = function(exchange) {
        // clearing previous search
        var formSearch = document.getElementById("coinPriceXrp")
@@ -101,9 +107,9 @@ var tickerXrp = function(){
            formSearch.parentNode.removeChild(formSearch);
    
        // creating dom to display on index html
-       var cryptoExchangeXrp = document.createElement("span")
+       var cryptoExchangeXrp = document.createElement("h5")
        cryptoExchangeXrp.id = "coinPriceXrp"
-       cryptoExchangeXrp.textContent = "$ " + exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+       cryptoExchangeXrp.textContent = "$ " + Math.round( (exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"])*10000)/10000;
        
        // attaching crypto close on html container
        cryptoExchangeElXrp.appendChild(cryptoExchangeXrp);
@@ -112,17 +118,93 @@ var tickerXrp = function(){
        console.log(cryptoExchangeXrp);
    }
 
+       // Teather ticker
+var tickerUsdt = function(){
+    var realTimeApiUrl = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USDT&to_currency=USD&apikey=U2BVTTCYEI0FHFAD`
+   fetch(realTimeApiUrl)
+   .then(function(response) {
+       // request was successful          
+       response.json().then(function(exchange){ 
+           // verifing dat is correct in console  
+           displayExchangeUsdt(exchange)
+           console.log(exchange);                
+    });  
+    // catch for lost of internet  
+   }).catch(function(error) {
+       alert("Unable to connect to server at this time.")
+   })
+   }
+
+   // Creating html object to display
+   var displayExchangeUsdt = function(exchange) {
+       // clearing previous search
+       var formSearch = document.getElementById("coinPriceUsdt")
+           if (formSearch)
+           formSearch.parentNode.removeChild(formSearch);
+   
+       // creating dom to display on index html
+       var cryptoExchangeUsdt = document.createElement("h5")
+       cryptoExchangeUsdt.id = "coinPriceUsdt"
+       cryptoExchangeUsdt.textContent = "$ " + Math.round( (exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"])*10000)/10000;
+       
+       // attaching crypto close on html container
+       cryptoExchangeElUsdt.appendChild(cryptoExchangeUsdt);
+   
+       // verifing closing price recieved in console 
+       console.log(cryptoExchangeUsdt);
+   }
+
+   // Litecoin ticker
+   var tickerLtc = function(){
+    var realTimeApiUrl = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=LTC&to_currency=USD&apikey=U2BVTTCYEI0FHFAD`
+   fetch(realTimeApiUrl)
+   .then(function(response) {
+       // request was successful          
+       response.json().then(function(exchange){ 
+           // verifing dat is correct in console  
+           displayExchangeLtc(exchange)
+           console.log(exchange);                
+    });  
+    // catch for lost of internet  
+   }).catch(function(error) {
+       alert("Unable to connect to server at this time.")
+   })
+   }
+   
+   // Creating html object to display
+   var displayExchangeLtc = function(exchange) {
+       // clearing previous search
+       var formSearch = document.getElementById("coinPriceLtc")
+           if (formSearch)
+           formSearch.parentNode.removeChild(formSearch);
+   
+       // creating dom to display on index html
+       var cryptoExchangeLtc = document.createElement("h5")
+       cryptoExchangeLtc.id = "coinPriceLtc"
+       cryptoExchangeLtc.textContent = "$ " + Math.round( (exchange["Realtime Currency Exchange Rate"]["5. Exchange Rate"])*10000)/10000;
+       
+       // attaching crypto close on html container
+       cryptoExchangeElLtc.appendChild(cryptoExchangeLtc);
+   
+       // verifing closing price recieved in console 
+       console.log(cryptoExchangeLtc);
+   }
+
 // Intial call
 tickerBtc();
 tickerEth();
 tickerXrp();
+tickerUsdt();
+tickerLtc();
 
 // refresh
 var refresh = function () {
     tickerBtc();
     setTimeout(tickerEth, 10000);
-    setTimeout(tickerXrp, 20000);    
+    setTimeout(tickerXrp, 20000);  
+    setTimeout(tickerUsdt, 30000); 
+    setTimeout(tickerLtc, 40000);   
 };
-var timer = setInterval(refresh, 30000);
+var timer = setInterval(refresh, 50000);
 
 
